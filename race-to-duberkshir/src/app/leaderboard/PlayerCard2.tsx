ee038c5fc90321e8c155e80e25a6305787e6a02b
Chris obrien <chrisobrien88@gmail.com>
2023-07-13 19:42:44 +0200

    prettier added to eslintrc.json

--- race-to-duberkshir/src/app/leaderboard/PlayerCard2.tsx
+++ race-to-duberkshir/src/app/leaderboard/PlayerCard2.tsx
@@ -20,8 +20,30 @@ interface PlayerCardProps {
 
 const PlayerCard = ({ player, index }: PlayerCardProps) => {
   return (
-    <div className=""
-    >{player.lastName}</div>
+    <div className="m-4 p-3 flex flex-row justify-around items-center bg-slate-200 hover:bg-slate-100 rounded-lg hover:rounded-2xl transition-all duration-200 ease-linear cursor-pointer group">
+      <p>{index + 1}</p>
+      <Image
+        src={player.profilePic}
+        alt={`${player.firstName} having a laugh`}
+        height={100}
+        width={100}
+        className="p-0 rounded-full"
+      />
+      <p className="text-slate-500 p-4">
+        {player.firstName} {player.lastName}
+      </p>
+      <p className="text-slate-700 p-1">{player.totalPoints} pts</p>
+
+      <div className="flex flex-col scale-0 group-hover:scale-100 transition-all duration-100 ease-in">
+        <p className="text-slate-500 p-1">{player.handicapIndex} handicap</p>
+        <p className="text-slate-500 p-1">
+          Rounds played: {player.roundsPlayed}
+        </p>
+        <p className="text-slate-500 p-1">
+          Avg score (all rounds): {player.allRoundsAverage}
+        </p>
+      </div>
+    </div>
   );
 };
 
